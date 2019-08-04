12b87e9b7a390b766737a3dd80554994f7e17020
jfcaballero <jfcp@inesingenieros.com>
2019-08-04 11:00:06 +0200

    add guard to myteseo page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthGuardService } from './auth-guard/auth-guard.service';
 
 
 
@@ -7,6 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [AuthGuardService]
 })
 export class CoreModule { }
--- src/app/myteseo/myteseo-routing.module.ts
+++ src/app/myteseo/myteseo-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from '../layout/main-layout/main-layout.component';
 import { MyteseoContainerComponent } from './myteseo-container/myteseo-container.component';
+import { AuthGuardService } from '../core/auth-guard/auth-guard.service';
 
 const myteseoRoutes: Routes = [
     {
         path: 'myteseo',
         component: MainLayoutComponent,
         children: [
-            { path: '', pathMatch: 'full', component: MyteseoContainerComponent }
+            { path: '', pathMatch: 'full', component: MyteseoContainerComponent, canActivate: [AuthGuardService] }
         ]
     }
 ];
