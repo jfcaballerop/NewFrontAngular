5d4fe7b790d9b2a75795f9d59db4e0537dd42492
Jose Caballero <josefernando.caballero@logesta.com>
2019-08-07 09:49:36 +0200

    change url prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiAuthUrl: 'http://www.mocky.io/v2/5d441bfb2f0000fc0c1794a5?mocky-delay=2s',
-  apiTwpermissionUrl: 'http://www.mocky.io/v2/5d4895e7330000f343a3efac?mocky-delay=1s'
+  apiAuthUrl: 'https://www.mocky.io/v2/5d441bfb2f0000fc0c1794a5?mocky-delay=2s',
+  apiTwpermissionUrl: 'https://www.mocky.io/v2/5d4895e7330000f343a3efac?mocky-delay=1s'
 };
