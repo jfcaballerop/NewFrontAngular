beb2b88045e7f2fff3555d73b0c440743c7853fc
Jose Caballero <josefernando.caballero@logesta.com>
2019-09-06 10:44:10 +0200

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  apiAuthUrl: 'https://www.mocky.io/v2/5d441bfb2f0000fc0c1794a5?mocky-delay=2s',
+  HOST_AUTH: 'vigiplus.logesta.com',
+  PORT_AUTH: '9016',
+  apiAuthUrl: 'http://vigiplus.logesta.com:9016/signin',
   apiTwpermissionUrl: 'https://www.mocky.io/v2/5d4895e7330000f343a3efac?mocky-delay=1s',
   apiTwPermissionProfileUrl: 'https://www.mocky.io/v2/5d4c3457310000ba15a952c0?mocky-delay=500ms',
   apiMyJsonServerBase: 'https://my-json-server.typicode.com/jfcaballerop/my-json-server'
