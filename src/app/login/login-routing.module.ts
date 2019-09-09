e7c4bda3049e66d07a9be8198418e86aa2c169b6
irodriguezr <ismael.rodriguez@logesta.com>
2019-09-09 17:25:41 +0200

    Login restyling

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginCardComponent } from './login-card/login-card.component';
 import { LoginContainerComponent } from './login-container/login-container.component';
+import { LoginLogestaComponent } from './login-logesta/login-logesta.component';
 
+// const loginRoutes: Routes = [
+//     {
+//         path: 'login',
+//         component: LoginContainerComponent,
+//         children: [
+//             { path: '', component: LoginCardComponent, pathMatch: 'full' }
+//         ]
+//     }
+// ];
 const loginRoutes: Routes = [
     {
         path: 'login',
-        component: LoginContainerComponent,
-        children: [
-            { path: '', component: LoginCardComponent, pathMatch: 'full' }
-        ]
+        component: LoginLogestaComponent,
+        // children: [
+        //     { path: '', component: LoginCardComponent, pathMatch: 'full' }
+        // ]
     }
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { SharedModule } from '../shared/shared.module';
-import { LoginCardComponent } from './login-card/login-card.component';
-import { LoginContainerComponent } from './login-container/login-container.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginLogestaComponent } from './login-logesta/login-logesta.component';
 
 
 
 @NgModule({
-  declarations: [LoginCardComponent, LoginContainerComponent],
+  declarations: [LoginLogestaComponent],
   imports: [
     CommonModule,
     SharedModule,
