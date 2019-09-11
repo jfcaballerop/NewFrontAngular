0aa48a00fb0edd0adbe148cc23ca00b8fdbe0b66
Jose Caballero <josefernando.caballero@logesta.com>
2019-09-11 14:07:34 +0200

    change Auth URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  HOST_AUTH: 'vigiplus.logesta.com',
-  PORT_AUTH: '9016',
-  apiAuthUrl: 'http://vigiplus.logesta.com:9016/signin',
+  HOST_AUTH: 'myteseo.dev.logesta.com',
+  PORT_AUTH: '7777',
+  apiAuthUrl: 'http://myteseo.dev.logesta.com:7777/signin',
   apiTwpermissionUrl: 'https://www.mocky.io/v2/5d4895e7330000f343a3efac?mocky-delay=1s',
   apiTwPermissionProfileUrl: 'https://www.mocky.io/v2/5d4c3457310000ba15a952c0?mocky-delay=500ms',
   apiMyJsonServerBase: 'https://my-json-server.typicode.com/jfcaballerop/my-json-server',
